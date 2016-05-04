784f78a28c014c729d1aa3dae143a97a0c6f4c03
Markus Kokott <markus.kokott@gmail.com>
2016-05-04 16:58:12 +0200

    stuff I forgot a minute ago

--- packer/prepare-workspace.sh
+++ packer/prepare-workspace.sh
@@ -3,10 +3,8 @@
 SCRIPT_DIR=$(dirname "$0")
 OS_DIR=$SCRIPT_DIR/os
 
-if [ ! -d $OS_DIR ]; then
-    mkdir $OS_DIR
+if [ ! -f $OS_DIR/ubuntu-16.04-server-amd64.iso ]; then
+    pushd $OS_DIR
+    wget http://de.releases.ubuntu.com/16.04/ubuntu-16.04-server-amd64.iso
+    popd
 fi
-
-pushd $OS_DIR
-wget http://de.releases.ubuntu.com/16.04/ubuntu-16.04-server-amd64.iso
-popd
